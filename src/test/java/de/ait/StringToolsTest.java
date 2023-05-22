aea590ab63097f107eb7c186114d6a0d3a930320
Jurgita Butiene <jurgita02@yahoo.com>
2023-05-22 23:18:51 +0200

    add display Name

--- src/test/java/de/ait/StringToolsTest.java
+++ src/test/java/de/ait/StringToolsTest.java
@@ -1,28 +1,48 @@
 package de.ait;
 
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringToolsTest {
-@Test
-    public void FirstToUpperCase_UpperFirstReturned(){
-    String actual=StringTools.FirstToUpperCase("apple");
-    assertEquals("Apple", actual);
 
-    String actual1 =StringTools.FirstToUpperCase(" ");
-    assertEquals("", actual1);
+    @Nested
+    @DisplayName("Testi dlia metoda FirstToUpperCase")
+
+    class ForFirstToUpperCaseTests {
+
+        @Test
+        public void FirstToUpperCase_UpperFirstReturned() {
+            String actual = StringTools.FirstToUpperCase("apple");
+            assertEquals("Apple", actual);
+        }
+            @Test
+            public void FirstToUpperCaseWithEmpty_UpperFirstReturned() {
+            String actual1 = StringTools.FirstToUpperCase("");
+            assertEquals("", actual1);
+        }
     }
 
-    @Test
-   public void isStartingFromCapital_TrueReturned(){
-     String actual= "apple";
-     boolean res=StringTools.isStartingFromCapital(actual);
-     assertFalse(res);
+    @Nested
+    @DisplayName("Testi dlia metoda isStartingFromCapital")
 
-        String actual1 = "Apple";
-        boolean res2 = StringTools.isStartingFromCapital(actual1);
-        assertTrue(res2);
-   }
+    class ForIsStartingFromCapitalTests {
+        @Test
+        @DisplayName("isStratringFromCapital(\"apple\") -> false")
+        public void isStartingFromCapital_TrueReturned() {
+            String actual = "apple";
+            boolean res = StringTools.isStartingFromCapital(actual);
+            assertFalse(res);
+        }
+            @Test
+            public void isStartingFromCapitalWithCapital_TrueReturned(){
 
+            String actual1 = "Apple";
+            boolean res2 = StringTools.isStartingFromCapital(actual1);
+            assertTrue(res2);
+        }
+    }
 }
