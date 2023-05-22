5077b988e41cb327d40eb99e85547d7d8e0e5189
Jurgita Butiene <jurgita02@yahoo.com>
2023-05-22 23:21:10 +0200

    add comment

--- src/test/java/de/ait/StringToolsTest.java
+++ src/test/java/de/ait/StringToolsTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringToolsTest {
-
+//aaaaaaaaa
     @Nested
     @DisplayName("Testi dlia metoda FirstToUpperCase")
 
